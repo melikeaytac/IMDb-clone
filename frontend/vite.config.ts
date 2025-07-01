fd8fa76001d16c10bc522d4ee47d9593c133c148
melikeaytac <48melikeaytac@gmail.com>
2025-07-02 01:21:52 +0300

    allowedhost

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -17,6 +15,9 @@ export default defineConfig({
   },
   server: {
     host: '0.0.0.0',
-    port: 5173 
+    port: 5173
+  },
+  preview: {
+    allowedHosts: ['imdb-clone-frontend-j076.onrender.com']
   }
 })
